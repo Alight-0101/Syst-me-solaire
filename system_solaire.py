195deb9cfefde555fdb0f4dc72790928aacf3958
Alight-0101 <58829839+Alight-0101@users.noreply.github.com>
2019-12-31 23:27:39 +0100

    Update system_solaire.py

--- system_solaire.py
+++ system_solaire.py
@@ -1,99 +1,127 @@
-from turtle import Shape, Turtle, mainloop, Vec2D as Vec # code de départ 
+from turtle import Shape, Turtle, mainloop, Vec2D as Vec
 from time import sleep
-#Turtle.bgcolor("black")
+
 G = 8
-class Gravity(object):
+
+class GravSys(object):
     def __init__(self):
         self.planets = []
-        self.time = 0
-        self.distance = 0.1
+        self.t = 0
+        self.dt = 0.01
     def init(self):
-        for planet in self.planets:
-            planet.init()
+        for p in self.planets:
+            p.init()
     def start(self):
-        for i in range(500):
-            self.time += self.distance
-            for planet in self.planets:
-                planet.step()
-class Planet(Turtle):
-    def __init__(self, mass, x_pos, velocity, gravity, shape):
+        for i in range(10000):
+            self.t += self.dt
+            for p in self.planets:
+                p.step()
+
+class Star(Turtle):
+    def __init__(self, m, x, v, gravSys, shape):
         Turtle.__init__(self, shape=shape)
         self.penup()
-        self.mass = mass
-        self.setpos(x_pos)
-        self.velocity = velocity
-        gravity.planets.append(self)
-        self.gravity = gravity
+        self.m = m
+        self.setpos(x)
+        self.v = v
+        gravSys.planets.append(self)
+        self.gravSys = gravSys
         self.resizemode("user")
         self.pendown()
     def init(self):
-        distance = self.gravity.distance
+        dt = self.gravSys.dt
         self.a = self.acc()
-        self.velocity = self.velocity + 0.5 * distance * self.a
+        self.v = self.v + 0.5*dt*self.a
     def acc(self):
         a = Vec(0,0)
-        for planet in self.gravity.planets:
+        for planet in self.gravSys.planets:
             if planet != self:
-                velocity = planet.pos()-self.pos()
-                a += (G*planet.mass/abs(velocity)**3)*velocity
+                v = planet.pos()-self.pos()
+                a += (G*planet.m/abs(v)**3)*v
         return a
     def step(self):
-        distance = self.gravity.distance
-        self.setpos(self.pos() + distance * self.velocity)
-        if self.gravity.planets.index(self) != 0:
-            self.setheading(self.towards(self.gravity.planets[0]))
+        dt = self.gravSys.dt
+        self.setpos(self.pos() + dt*self.v)
+        if self.gravSys.planets.index(self) != 0:
+            self.setheading(self.towards(self.gravSys.planets[0]))
         self.a = self.acc()
-        self.velocity = self.velocity + distance * self.a
+        self.v = self.v + dt*self.a
+
 ## create compound yellow/blue turtleshape for planets
+
 def main():
     s = Turtle()
     s.reset()
     s.getscreen().tracer(0,0)
-    s.ht() #hide turtle
-    s.pu() #pen up
-    s.fd(6) # forward
-    s.lt(90) #lt
-    s.begin_poly() #begining of the polygon, the current position is the first vecto of the polygon
-    s.circle(5) 
-    s.end_poly() #end the polygon
-    #creates m1, creats a semi circle  
+    s.ht()
+    s.pu()
+    s.fd(6)
+    s.lt(90)
+    s.begin_poly()
+    s.circle(6, 180)
+    s.end_poly()
     m1 = s.get_poly()
     s.begin_poly()
-    s.circle(5)
+    s.circle(6,180)
     s.end_poly()
-    #creates m2, uses the specs from the last recorded poly ^
-    #m2 = s.get_poly()
+    m2 = s.get_poly()
+
     planetshape = Shape("compound")
-    planetshape.addcomponent(m1,"green")
+    planetshape.addcomponent(m1,"orange")
+    planetshape.addcomponent(m2,"blue")
     s.getscreen().register_shape("planet", planetshape)
     s.getscreen().tracer(1,0)
+
     ## setup gravitational system
-    gs = Gravity()
-    sun = Planet(1000000, Vec(0,0), Vec(0,0), gs, "circle")
+    gs = GravSys()
+    sun = Star(1000000, Vec(0,0), Vec(0,0), gs, "circle")
     sun.color("yellow")
-    sun.shapesize(5)
+    sun.shapesize(1.8)
     sun.pu()
-    earth = Planet(12500, Vec(215,0), Vec(0,195), gs, "planet")
+    
+    mercure = Star(11500, Vec(100,0), Vec(0,195), gs, "planet")
+    mercure.pencolor("green")
+    mercure.shapesize(0.4)
+    
+    vénus = Star(11100, Vec(150,0), Vec(0,195), gs, "planet")#utilsation de la classe planet avec les paramètres 
+    vénus.pencolor("pink")#changer la couleur
+    vénus.shapesize(0.6)# taille de la boule
+    
+    earth = Star(12500, Vec(200,0), Vec(0,195), gs, "planet")
     earth.pencolor("green")
-    earth.shapesize(2)
-    moon = Planet(1, Vec(225,0), Vec(0,295),gs, 'planet')
-    moon.pencolor('blue')
-    moon.shapesize(1)
-    mars = Planet(4000, Vec(0,327), Vec(150,0), gs, "planet")
-    mars.pencolor('red')
-    mars.shapesize(2)
-    jupiter = Planet(750, Vec(-430,0), Vec(0,100), gs, "planet")
-    jupiter.pencolor('purple')
-    jupiter.shapesize(3)
- #   p5 = Planet (mass, x_pos, velocity, gravity, shape)
- #   p6 = (name, radius), mass, colour, distance, x velocity, y velocity
+    earth.shapesize(0.8)
+    
+    moon = Star(1, Vec(210,0), Vec(0,295), gs, "planet")
+    moon.pencolor("blue")
+    moon.shapesize(0.5)
+    
+    mars = Star(40000, Vec(320,0), Vec(0,195), gs, "planet")#utilsation de la classe planet avec les paramètres 
+    mars.pencolor('red')#changer la couleur
+    mars.shapesize(2)# taille de la boule
+    
+    jupiter = Star(75000, Vec(430,0), Vec(-50,300), gs, "planet")#utilsation de la classe planet avec les paramètres 
+    jupiter.pencolor('purple')#changer la couleur
+    jupiter.shapesize(3)# taille de la boule
+    
+    saturne  = Star(76500, Vec(500,0), Vec(0,195), gs, "planet")#utilsation de la classe planet avec les paramètres 
+    saturne.pencolor("yellow")#changer la couleur
+    saturne.shapesize(2)# taille de la boule
+    
+    uranus = Star(12500, Vec(570,0), Vec(0,195), gs, "planet")#utilsation de la classe planet avec les paramètres 
+    uranus.pencolor("green")#changer la couleur
+    uranus.shapesize(2)# taille de la boule
+    
+    neptune = Star(12500, Vec(650,0), Vec(0,195), gs, "planet")#utilsation de la classe planet avec les paramètres 
+    neptune.pencolor("green")#changer la couleur
+    neptune.shapesize(2)# taille de la boule
+    
     gs.init()
     gs.start()
-#   s.onclick(pause)
     return "Done!"
+
 if __name__ == '__main__':
     msg = main()
-    print(msg)
+    print (msg)
     mainloop()
 
 print(**********************************************************************************************************************************)
